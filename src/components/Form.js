674da7162ab27a0262f6afd727e1b6605f8c6d4c
Michael Kolawole <Michael.Kolawole@gmail.com>
2021-02-17 15:19:19 +0000

    first draft completed

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,8 @@
 import React from 'react';
 
-const Form = ({ setInputText, todos, setTodos, inputText }) => {
+const Form = ({ setInputText, todos, setTodos, inputText, setStatus }) => {
 
     const inputTextHandler = (e) => {
-        console.log(e.target.value);
         setInputText(e.target.value);
     };
 
@@ -16,29 +15,33 @@ const Form = ({ setInputText, todos, setTodos, inputText }) => {
         setInputText("");
     }
 
-    return(
-        <form>
-            <input
-                value={inputText} 
-                onChange={inputTextHandler}
-                type="text" 
-                className="todo-input" 
-            />
-            <button 
-                onClick={submitTodoHandler} 
-                className="todo-button" 
-                type="submit"
-            >
-                <i className="fas fa-plus-square"></i>
-            </button>
-            <div className="select">
-                <select name="todos" className="filter-todo">
-                    <option value="all">All</option>
-                    <option value="completed">Completed</option>
-                    <option value="uncompleted">Uncompleted</option>
-                </select>
-            </div>
-        </form>
+    const statusHandler = (e) => {
+        setStatus(e.target.value)
+    }
+
+    return (
+      <form>
+        <input
+          value={inputText}
+          onChange={inputTextHandler}
+          type="text"
+          className="todo-input"
+        />
+        <button
+          onClick={submitTodoHandler}
+          className="todo-button"
+          type="submit"
+        >
+          <i className="fas fa-plus-square"></i>
+        </button>
+        <div className="select">
+          <select onChange={statusHandler} name="todos" className="filter-todo">
+            <option value="all">All</option>
+            <option value="completed">Completed</option>
+            <option value="uncompleted">Uncompleted</option>
+          </select>
+        </div>
+      </form>
     );
 };
 
