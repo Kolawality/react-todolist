d901b1b8a62dce79584a60ef42e00352ad4a6aa2
Michael Kolawole <Michael.Kolawole@gmail.com>
2021-02-17 09:03:54 +0000

    started adding the todo components to the todo list

--- src/App.js
+++ src/App.js
@@ -6,14 +6,20 @@ import TodoList from './components/TodoList';
 function App() {
 
   const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
 
   return (
     <div className="App">
       <header>
         <h1>Michael's Todo List</h1>        
       </header>
-      <Form />
-      <TodoList />
+      <Form 
+        inputText={inputText}
+        setInputText={setInputText}
+        todos={todos}
+        setTodos={setTodos}
+      />
+      <TodoList todos={todos}/>
     </div>    
   );
 }
